b1c439f455feaa51b65070e5dae6ad0024fcd94c
krasik <classyvanswangsit@gmail.com>
2017-11-10 13:35:28 +0700

    add junit test for class "Songsakdi"

---
+++ LabGit/src/controller/TestKaj.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestKaj {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
